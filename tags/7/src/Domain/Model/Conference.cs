53e5dbeb874432f6c8c7061ddbb96e507ba16667
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2007-12-27 03:35:40 +0000

    CCNET build 7

---
+++ tags/7/src/Domain/Model/Conference.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using Iesi.Collections.Generic;
+
+namespace CodeCampServer.Domain.Model
+{
+    public class Conference : EntityBase
+    {
+        private string _key;
+        private string _name;
+        private string _description;
+        private DateTime? _startDate;
+        private DateTime? _endDate;
+        private string _sponsorInfo;
+        private Location _location = new Location();
+        private int _maxAttendees;
+        private ISet<TimeSlot> _timeSlots = new HashedSet<TimeSlot>();
+        private ISet<ConfirmedSponsor> _sponsors = new HashedSet<ConfirmedSponsor>();
+
+        public Conference()
+        {
+        }
+
+        public Conference(string key, string name)
+        {
+            _key = key;
+            _name = name;
+        }
+
+        public virtual string Key
+        {
+            get { return _key; }
+            set { _key = value; }
+        }
+
+        public virtual string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public virtual string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public virtual DateTime? StartDate
+        {
+            get { return _startDate; }
+            set { _startDate = value; }
+        }
+
+        public virtual DateTime? EndDate
+        {
+            get { return _endDate; }
+            set { _endDate = value; }
+        }
+
+        public virtual string SponsorInfo
+        {
+            get { return _sponsorInfo; }
+            set { _sponsorInfo = value; }
+        }
+
+        public virtual Location Location
+        {
+            get { return _location; }
+        }
+
+        public virtual int MaxAttendees
+        {
+            get { return _maxAttendees; }
+            set { _maxAttendees = value; }
+        }
+
+        public virtual void AddTimeSlot(DateTime startTime, DateTime endTime)
+        {
+            if(startTime < StartDate || endTime > EndDate)
+            {
+                throw new Exception("Time slot must be within conference.");
+            }
+
+            _timeSlots.Add(new TimeSlot(startTime, endTime));
+        }
+
+        public virtual TimeSlot[] TimeSlots
+        {
+            get { return new List<TimeSlot>(_timeSlots).ToArray(); }
+        }
+
+        public virtual ConfirmedSponsor[] GetSponsors()
+        {
+            return new List<ConfirmedSponsor>(_sponsors).ToArray();
+        }
+
+        public virtual void AddSponsor(Sponsor sponsor, SponsorLevel level)
+        {
+            _sponsors.Add(new ConfirmedSponsor(sponsor, level));
+        }
+
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+}
---
+++ tags/7/src/IntegrationTests/DataAccess/ZDataLoader.cs
@@ -0,0 +1,50 @@
+using System;
+using CodeCampServer.DataAccess;
+using CodeCampServer.Domain.Model;
+using NHibernate;
+using NUnit.Framework;
+
+namespace CodeCampServer.IntegrationTests.DataAccess
+{
+    [TestFixture]
+    public class ZDataLoader : DatabaseTesterBase
+    {
+        [Test, Category("DataLoader")]
+        public void PopulateDatabase()
+        {
+            Conference codeCamp2007 =
+                new Conference("codecamp2007", "Uber Code Camp 2007");
+            codeCamp2007.StartDate = new DateTime(2007, 11, 26, 19, 30, 00);
+            Conference codeCamp2008 = new Conference("codecamp2008", "Uber Code Camp 2008");
+            codeCamp2008.StartDate = new DateTime(2008, 4, 13, 19, 0, 0);
+            Conference codeCamp2009 = new Conference("codecamp2009", "Uber Code Camp 2009");
+            codeCamp2009.StartDate = new DateTime(2009, 6, 8, 19, 0, 0);
+
+            Attendee attendee1 =
+                new Attendee("Homer", "Simpson", "http://www.simpsons.com", "Doh!", codeCamp2007, "a@b.com");
+            Attendee attendee2 =
+                new Attendee("Bart", "Simpson", "http://www.simpsons.com", "Eat my shorts", codeCamp2007,
+                             "a@b.com");
+            Attendee attendee3 =
+                new Attendee("Marge", "Simpson", "http://www.simpsons.com", "MMmmmm", codeCamp2007, "a@b.com");
+
+            using (ISession session = getSession(Database.Default))
+            {
+                session.SaveOrUpdate(codeCamp2007);
+                session.SaveOrUpdate(codeCamp2008);
+                session.SaveOrUpdate(codeCamp2009);
+                session.SaveOrUpdate(attendee1);
+                session.SaveOrUpdate(attendee2);
+                session.SaveOrUpdate(attendee3);
+
+                session.Flush();
+            }
+        }
+
+        [Test, Category("CreateSchema")]
+        public void RecreateDatabaseSchema()
+        {
+            recreateDatabase(Database.Default);
+        }
+    }
+}
