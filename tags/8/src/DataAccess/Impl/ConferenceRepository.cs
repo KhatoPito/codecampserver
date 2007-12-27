f0c42f5ce6ff261a4cc55a5a85b634b1fd30bf28
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2007-12-27 05:02:19 +0000

    CCNET build 8

---
+++ tags/8/src/DataAccess/Impl/ConferenceRepository.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using CodeCampServer.DataAccess.Impl;
+using CodeCampServer.Domain;
+using CodeCampServer.Domain.Model;
+using NHibernate;
+using StructureMap;
+
+namespace CodeCampServer.DataAccess.Impl
+{
+    [Pluggable("Default")]
+    public class ConferenceRepository : RepositoryBase, IConferenceRepository
+    {
+        public ConferenceRepository(ISessionBuilder sessionFactory) : base(sessionFactory)
+        {
+        }
+
+        public IEnumerable<Conference> GetAllConferences()
+        {
+            using (ISession session = getSession(Database.Default))
+            {
+                IQuery query = session.CreateQuery("from Conference");
+
+                IEnumerable<Conference> result = query.List<Conference>();
+
+                return result;
+            }
+        }
+
+        public Conference GetConferenceByKey(string key)
+        {
+            using (ISession session = getSession(Database.Default))
+            {
+                IQuery query = session.CreateQuery("from Conference e where e.Key = ?");
+                query.SetParameter(0, key);
+                Conference result = query.UniqueResult<Conference>();
+                return result;
+            }
+        }
+
+        public Conference GetFirstConferenceAfterDate(DateTime date)
+        {
+            using(ISession session = getSession(Database.Default))
+            {
+                IQuery query = session.CreateQuery("from Conference e where e.StartDate >= ? order by e.StartDate asc");
+                query.SetParameter(0, date);
+                query.SetMaxResults(1);
+                Conference matchingConference = query.UniqueResult<Conference>();
+                return matchingConference;
+            }
+        }
+
+        public Conference GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Conference conference)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tags/8/src/Domain/IConferenceRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using CodeCampServer.Domain.Model;
+using StructureMap;
+
+namespace CodeCampServer.Domain
+{
+    [PluginFamily("Default")]
+    public interface IConferenceRepository
+    {
+        IEnumerable<Conference> GetAllConferences();
+        Conference GetConferenceByKey(string key);
+        Conference GetFirstConferenceAfterDate(DateTime date);
+        Conference GetById(Guid id);
+        void Save(Conference conference);
+    }
+}
