b6ec6769877db52eae83b4aa14118dd897c52f33
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2007-12-30 18:01:31 +0000

    CCNET build 21

---
+++ tags/21/src/Model/Domain/OnlineResource.cs
@@ -0,0 +1,27 @@
+namespace CodeCampServer.Model.Domain
+{
+    public class OnlineResource
+    {
+        private OnlineResourceType _type;
+        private string _name;
+        private string _href;
+
+        public virtual OnlineResourceType Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public virtual string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public virtual string Href
+        {
+            get { return _href; }
+            set { _href = value; }
+        }
+    }
+}
---
+++ tags/21/src/Website/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using CodeCampServer.Website.Controllers;
+using MvcContrib.ControllerFactories;
+using StructureMap;
+using CodeCampServer.Website.Views;
+
+namespace CodeCampServer.Website
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            InitializeControllerFactory();
+            SetupRoutes();
+        }
+
+        private void InitializeControllerFactory()
+        {
+            ControllerBuilder.Current.SetDefaultControllerFactory(typeof(StructureMapControllerFactory));
+            StructureMapConfiguration.BuildInstancesOf<ConferenceController>()
+                .TheDefaultIsConcreteType<ConferenceController>();
+        }
+
+        private void SetupRoutes()
+        {
+            RouteManager.RegisterRoutes(RouteTable.Routes);            
+        }
+    }
+}
