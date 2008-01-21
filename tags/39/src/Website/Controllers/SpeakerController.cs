21081a99d1cb97633cd4ddb971c9996a0d38677b
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-01-21 03:03:00 +0000

    CCNET build 39

---
+++ tags/39/src/Website/Controllers/SpeakerController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using CodeCampServer.Model;
+using CodeCampServer.Model.Domain;
+using CodeCampServer.Model.Exceptions;
+using CodeCampServer.Model.Presentation;
+using CodeCampServer.Website.Models.Speaker;
+
+namespace CodeCampServer.Website.Controllers
+{
+    public class SpeakerController : Controller
+    {
+        private readonly IConferenceService _conferenceService;
+        private IClock _clock;
+
+        public SpeakerController(IConferenceService conferenceService, IClock clock)
+        {            
+            _conferenceService = conferenceService;
+            _clock = clock;
+        }
+
+        [ControllerAction]
+        public void Index(string conferenceKey)
+        {
+            RedirectToAction( new
+            {
+                Controller="conference", Action="details", conferenceKey=conferenceKey
+            });            
+        }
+
+        [ControllerAction]
+        public void List(string conferenceKey, int? page, int? perPage)
+        {
+            int effectivePage = page.GetValueOrDefault(0);
+            int effectivePerPage = perPage.GetValueOrDefault(20);
+
+            Conference conference = _conferenceService.GetConference(conferenceKey);
+            ScheduledConference scheduledConference = new ScheduledConference(conference, _clock);
+            IEnumerable<Speaker> speakers = _conferenceService.GetSpeakers(conference, effectivePage, effectivePerPage);
+            IEnumerable<SpeakerListing> speakerListings = new SpeakerListingCollection(speakers);
+            
+            ListSpeakersViewData viewData = new ListSpeakersViewData(scheduledConference, speakerListings, effectivePage, effectivePerPage);
+
+            RenderView("List", viewData);
+        }
+
+        [ControllerAction]
+        public void View(string conferenceKey, string speakerId)
+        {
+            Speaker speaker = _conferenceService.GetSpeakerByDisplayName(speakerId);
+            RenderView("view", speaker);
+        }
+
+        [ControllerAction]
+        public void Edit()
+        {
+            Speaker speaker = _conferenceService.GetLoggedInSpeaker();
+            if (speaker != null)
+            {                
+                RenderView("edit", speaker);
+            }
+            else
+            {
+                RedirectToAction(new { Controller="login" });
+            }
+        }
+
+        [ControllerAction]
+        public void Save(string conferenceKey, string displayName, string firstName, string lastName, string website, string comment, string profile, string avatarUrl)
+        {
+            string email = _conferenceService.GetLoggedInUsername();
+            try
+            {
+                Speaker savedSpeaker = _conferenceService.SaveSpeaker(email, firstName, lastName, website, comment, displayName, profile, avatarUrl);
+                
+                //TempData["message"] = "Profile saved";
+                RedirectToAction(new { Action = "view", conferenceKey = conferenceKey, speakerId = displayName });
+            }
+            catch (DataValidationException ex)
+            {
+                //TempData["error"] = ex.Message;
+                RedirectToAction("edit");
+            }
+            
+        }
+    }
+}
---
+++ tags/39/src/Website/Views/Conference/Details.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Layouts/TwoColumn.Master" AutoEventWireup="true" 
+	Inherits="CodeCampServer.Website.Views.ViewBase" %>
+<%@ Import namespace="CodeCampServer.Model.Domain"%>
+
+<asp:Content ContentPlaceHolderID="SidebarContentPlaceHolder" ID="SidebarContent" runat="server">
+
+<div>
+<%ScheduledConference conference = ViewData.Get<ScheduledConference>(); %>
+<%=Html.ActionLink("Register Now!", new{ action = "pleaseregister", conferenceKey = conference.Key}) %>
+<br />
+<%=Html.ActionLink("Schedule", new{ action = "schedule", conferenceKey = conference.Key}) %>
+<br />
+<%=Html.ActionLink("List Attendees", new{ action = "listattendees", conferenceKey = conference.Key}) %>
+</div>
+</asp:Content>
+
+
+<asp:Content ContentPlaceHolderID="MainContentPlaceHolder" ID="MainContent" runat="server">
+<%ScheduledConference conference = ViewData.Get<ScheduledConference>();%>
+Conference name: <%=conference.Name %>
+<br />
+
+<% if (conference.DaysUntilStart.HasValue) { %>
+There are <%=conference.DaysUntilStart.ToString()%> days remaining until the conference starts.
+<br />
+<% } %>
+
+
+Starts: <%=conference.StartDate.ToString()%>
+</asp:Content>
