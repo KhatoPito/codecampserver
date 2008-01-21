c61b3455aa529234ed8a5aba9575da2d3683c5b1
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-01-21 00:34:37 +0000

    CCNET build 38

---
+++ tags/38/src/UnitTests/Website/Views/SmartBagTester.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Security.Policy;
+using CodeCampServer.Website.Views;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace CodeCampServer.UnitTests.Website.Views
+{
+	[TestFixture]
+	public class SmartBagTester
+	{
+		[Test]
+		public void ShouldRetrieveSingleObjectByType()
+		{
+			SmartBag bag = new SmartBag();
+			Url url = new Url("/asdf"); //arbitrary object
+			bag.Add(url);
+
+			Assert.That(bag.Get<Url>(), Is.EqualTo(url));
+			Assert.That(bag.Get(typeof(Url)), Is.EqualTo(url));
+		}
+
+		[Test, ExpectedException(ExceptionType = typeof(ArgumentException), ExpectedMessage = "You can only add one default object for type 'System.Security.Policy.Url'.")]
+		public void AddingTwoDefaultObjectsOfSameTypeThrows()
+		{
+			Url url1 = new Url("/1");
+			Url url2 = new Url("/2");
+
+			SmartBag bag = new SmartBag();
+			bag.Add(url1);
+			bag.Add(url2);
+		}
+
+		[Test, ExpectedException(typeof(ArgumentException), ExpectedMessage = "No object exists with key 'System.Security.Policy.Url'.")]
+		public void ShouldGetMeaningfulExceptionIfObjectDoesntExist()
+		{
+			SmartBag bag = new SmartBag();
+			Url url = bag.Get<Url>();
+		}
+
+		[Test]
+		public void ShouldReportContainsCorrectly()
+		{
+			SmartBag bag = new SmartBag();
+			bag.Add(new Url("/2"));
+
+			Assert.That(bag.Contains<Url>());
+			Assert.That(bag.Contains(typeof(Url)));
+		}
+
+		[Test]
+		public void ShouldManageMoreThanOneObjectPerType()
+		{
+			SmartBag bag = new SmartBag();
+			bag.Add("key1", new Url("/1"));
+			bag.Add("key2", new Url("/2"));
+
+			Assert.That(bag.Get<Url>("key1").Value, Is.EqualTo("/1"));
+			Assert.That(bag.Get<Url>("key2").Value, Is.EqualTo("/2"));
+		}
+
+		[Test, ExpectedException(typeof(ArgumentException), ExpectedMessage = "No object exists with key 'foobar'.")]
+		public void ShouldGetMeaningfulExceptionIfObjectDoesntExistByKey()
+		{
+			SmartBag bag = new SmartBag();
+			Url url = bag.Get<Url>("foobar");
+		}
+
+		[Test]
+		public void ShouldCountNumberOfObjectsOfGivenType()
+		{
+			SmartBag bag = new SmartBag();
+			Assert.That(bag.GetCount(typeof(Url)), Is.EqualTo(0));
+
+			bag.Add("1", new Url("/1"));
+			bag.Add("2", new Url("/2"));
+			bag.Add("3", new Url("/3"));
+
+			Assert.That(bag.GetCount(typeof(Url)), Is.EqualTo(3));
+		}
+	}
+}
---
+++ tags/38/src/Website/Views/Conference/Details.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Layouts/TwoColumn.Master" AutoEventWireup="true" CodeBehind="Details.aspx.cs" Inherits="CodeCampServer.Website.Views.Conference.Details" %>
+<%@ Import namespace="CodeCampServer.Model.Domain"%>
+
+<asp:Content ContentPlaceHolderID="SidebarContentPlaceHolder" ID="SidebarContent" runat="server">
+
+<div>
+<%=Html.ActionLink("Register Now!", new{ action = "pleaseregister", conferenceKey = ViewData.Key}) %>
+<br />
+<%=Html.ActionLink("Schedule", new{ action = "schedule", conferenceKey = ViewData.Key}) %>
+<br />
+<%=Html.ActionLink("List Attendees", new{ action = "listattendees", conferenceKey = ViewData.Key}) %>
+</div>
+</asp:Content>
+
+
+<asp:Content ContentPlaceHolderID="MainContentPlaceHolder" ID="MainContent" runat="server">
+Conference name: <%=ViewData.Name %>
+<br />
+
+<% if (ViewData.DaysUntilStart.HasValue) { %>
+There are <%=ViewData.DaysUntilStart.ToString() %> days remaining until the conference starts.
+<br />
+<% } %>
+
+
+Starts: <%=ViewData.StartDate.ToString() %>
+</asp:Content>
