3e428afb4ba3a6f9ff6a3ca6325cc5e26edbbebb
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-01-23 05:11:22 +0000

    CCNET build 46

---
+++ tags/46/src/Model/IConferenceService.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using CodeCampServer.Model.Domain;
+using Iesi.Collections.Generic;
+using StructureMap;
+
+namespace CodeCampServer.Model
+{
+	[PluginFamily(Keys.DEFAULT)]
+	public interface IConferenceService
+	{
+		Conference GetConference(string conferenceKey);
+		Attendee[] GetAttendees(Conference conference, int page, int perPage);
+		Attendee RegisterAttendee(string firstName, string lastName, string website, string comment, Conference conference, string emailAddress, string cleartextPassword);
+
+        string GetLoggedInUsername();
+
+        Session CreateSession(Speaker speaker, string title, string @abstract, OnlineResource[] onlineResources);
+
+        Speaker GetLoggedInSpeaker();
+        Speaker GetSpeakerByDisplayName(string displayName);
+        Speaker GetSpeakerByEmail(string email);
+        IEnumerable<Speaker> GetSpeakers(Conference conference, int page, int perPage);
+        Speaker SaveSpeaker(string emailAddress, string firstName, string lastName, string website, string comment, string displayName, string profile, string avatarUrl);
+    }
+}
