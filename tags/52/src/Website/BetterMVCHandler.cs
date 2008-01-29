5e4987c690e2890f0cf9b7db64d2101161e2f5ee
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-01-29 02:40:31 +0000

    CCNET build 52

---
+++ tags/52/src/Website/BetterMVCHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeCampServer.Website
+{
+	public class BetterMvcHandler : MvcHandler
+	{
+		protected override void ProcessRequest(IHttpContext httpContext)
+		{
+			try
+			{
+				base.ProcessRequest(httpContext);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Route used:" + RequestContext.RouteData.Route.Url, ex);
+			}
+		}
+	}
+}
