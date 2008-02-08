d6e73b31e07f405d23a63b1432a97a455fbbf389
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-02-08 05:19:43 +0000

    CCNET build 61

---
+++ tags/61/src/Model/IConferenceService.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using CodeCampServer.Model.Domain;
+using StructureMap;
+
+namespace CodeCampServer.Model
+{
+	[PluginFamily(Keys.DEFAULT)]
+	public interface IConferenceService
+	{
+		Conference GetConference(string conferenceKey);
+        IEnumerable<Conference> GetAllConferences();
+        
+        Attendee[] GetAttendees(Conference conference, int page, int perPage);
+		Attendee RegisterAttendee(string firstName, string lastName, string website, string comment, Conference conference, string emailAddress, string cleartextPassword);
+
+        string GetLoggedInUsername();
+        Speaker GetLoggedInSpeaker();
+
+        Session CreateSession(Speaker speaker, string title, string @abstract, OnlineResource[] onlineResources);
+        IEnumerable<Session> GetProposedSessions(Conference conference);
+
+        Speaker GetSpeakerByDisplayName(string displayName);
+        Speaker GetSpeakerByEmail(string email);
+        IEnumerable<Speaker> GetSpeakers(Conference conference, int page, int perPage);
+        Speaker SaveSpeaker(string emailAddress, string firstName, string lastName, string website, string comment, string displayName, string profile, string avatarUrl);
+	}
+}
---
+++ tags/61/src/UnitTests/Website/Controllers/ApplicationControllerTests.cs
@@ -0,0 +1,117 @@
+﻿using System.Web.Mvc;
+using CodeCampServer.Model.Security;
+using CodeCampServer.Website.Controllers;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+
+namespace CodeCampServer.UnitTests.Website.Controllers
+{
+    [TestFixture]
+    public class ApplicationControllerTests
+    {
+        IAuthorizationService _authorizationService;
+
+        private class FooController : ApplicationController
+        {
+            public FooController(IAuthorizationService authorizationService) : base(authorizationService)
+            {
+            }
+
+            [ControllerAction]
+            public void Bar()
+            {
+                //would normally be called by the Execute method
+                OnPreAction("bar", null);
+            }
+        }
+
+        private MockRepository _mocks;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mocks = new MockRepository();
+            _authorizationService = _mocks.CreateMock<IAuthorizationService>();
+        }
+
+        [Test]
+        public void OnPreActionShouldVerifyTheUserIsAnAdmin()
+        {
+            Expect.Call(_authorizationService.IsAdministrator).Return(true);
+            _mocks.ReplayAll();
+
+            FooController controller = new FooController(_authorizationService);            
+            controller.Bar();
+            
+            _mocks.VerifyAll();
+        }
+
+        [Test]
+        public void OnPreActionShouldSetSmartBagDataToRenderAdminPanelWhenTheUserIsAnAdmin()
+        {
+            SetupResult.For(_authorizationService.IsAdministrator).Return(true);
+            _mocks.ReplayAll();
+
+            FooController controller = new FooController(_authorizationService);
+            controller.Bar();
+
+            Assert.That(controller.SmartBag.ContainsKey("ShouldRenderAdminPanel"));
+            Assert.That(controller.SmartBag.Get<bool>("ShouldRenderAdminPanel"), Is.True);
+
+            _mocks.BackToRecordAll();
+            SetupResult.For(_authorizationService.IsAdministrator).Return(false);
+            _mocks.ReplayAll();
+
+            controller.Bar();
+            _mocks.VerifyAll();
+        }
+
+
+        private class RenderTestController : ApplicationController
+        {
+            public object ActualViewData;
+
+            public RenderTestController(IAuthorizationService authorizationService) 
+                : base(authorizationService)
+            {
+            }
+
+            protected override void RenderView(string viewName, string masterName, object viewData)
+            {
+                ActualViewData = viewData;
+            }
+
+            [ControllerAction]
+            public void RenderWithViewName(string viewName)
+            {
+                RenderView(viewName);
+            }
+            [ControllerAction]
+            public void RenderWithViewNameAndMasterName(string viewName, string masterName)
+            {
+                RenderView(viewName, masterName);
+            }
+        }
+        
+        [Test]
+        public void RenderWithViewStringShouldUseSmartBagAndNotDefaultViewData()
+        {
+            RenderTestController controller = new RenderTestController(_authorizationService);
+            controller.RenderWithViewName("TestView");
+
+            Assert.That(controller.ActualViewData, Is.Null);
+        }
+
+        [Test]
+        public void RenderWithViewStringAndMasterNameShouldUseSmartBagAndNotDefaultViewData()
+        {
+            RenderTestController controller = new RenderTestController(_authorizationService);
+            controller.RenderWithViewNameAndMasterName("TestView", "Master");
+
+            Assert.That(controller.ActualViewData, Is.Null);
+        }
+    }
+
+
+}
