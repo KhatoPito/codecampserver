583e0f1d86bf66357980e4435e6cb6d1ba2e8b74
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-03-02 04:54:22 +0000

    CCNET build 71

---
+++ tags/71/src/Website/BetterMvcRouteHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CodeCampServer.Website
+{
+	public class BetterMvcRouteHandler : IRouteHandler
+	{
+		#region IRouteHandler Members
+
+		public IHttpHandler GetHttpHandler(RequestContext requestContext)
+		{
+			MvcHandler handler = new BetterMvcHandler();
+			handler.RequestContext = requestContext;
+			return handler;
+		}
+
+		#endregion
+	}
+}
