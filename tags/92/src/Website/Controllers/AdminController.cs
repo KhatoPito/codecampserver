6e305b29a7ee45b6de44292c670c67780f87ac5b
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-05-04 17:51:55 +0000

    CCNET build 92

---
+++ tags/92/src/Website/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using CodeCampServer.Model;
+using CodeCampServer.Model.Security;
+using CodeCampServer.Website.Views;
+
+namespace CodeCampServer.Website.Controllers
+{    
+    [AdminOnly]
+    public class AdminController : Controller
+    {
+        private readonly IConferenceService _conferenceService;        
+
+        public AdminController(IAuthorizationService authorizationService, IConferenceService conferenceService) : base(authorizationService)
+        {
+            _conferenceService = conferenceService;
+        }
+        
+        public ActionResult Index()
+        {            
+            var conferences = _conferenceService.GetAllConferences();
+            ViewData.Add(conferences);
+            return RenderView();
+        }
+        
+        public ActionResult Schedule()
+        {
+            return RenderView();
+        }
+    }
+}
