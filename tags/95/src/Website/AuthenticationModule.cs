4444f613f061de18ce01ce26bef3c1bd31382b95
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-05-07 15:28:22 +0000

    CCNET build 95

---
+++ tags/95/src/Website/AuthenticationModule.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Principal;
+using System.Web;
+using System.Web.Security;
+
+namespace CodeCampServer.Website
+{
+    public class AuthenticationModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.AuthenticateRequest += context_AuthenticateRequest;
+        }
+
+        void context_AuthenticateRequest(object sender, System.EventArgs e)
+        {
+            var context = ((HttpApplication) sender).Context;
+            if(context.User == null || !context.User.Identity.IsAuthenticated) return;
+
+            if(context.User.Identity is FormsIdentity)
+            {
+                var formsIdentity = (FormsIdentity) context.User.Identity;
+                var ticket = formsIdentity.Ticket;
+                var roles = ticket.UserData.Split(',');
+
+                context.User = new GenericPrincipal(formsIdentity, roles);
+            }
+
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
