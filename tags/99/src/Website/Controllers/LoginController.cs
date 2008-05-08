ee74b794167d4a6be79aab307152223cfbd20903
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-05-08 19:57:13 +0000

    CCNET build 99

---
+++ tags/99/src/Website/Controllers/LoginController.cs
@@ -0,0 +1,79 @@
+﻿using System.Security;
+using System.Web.Mvc;
+using CodeCampServer.Model;
+using CodeCampServer.Model.Domain;
+using CodeCampServer.Model.Security;
+using MvcContrib.Filters;
+
+namespace CodeCampServer.Website.Controllers
+{
+	public class LoginController : Controller
+	{
+		private readonly ILoginService _loginService;
+	    private readonly IPersonRepository _personRepository;
+	    private readonly IAuthenticationService _authenticationService;
+	    private readonly ICryptoUtil _cryptoUtil;
+
+	    public LoginController(ILoginService loginService, IPersonRepository personRepository, IAuthenticationService authenticationService, IAuthorizationService authService, ICryptoUtil cryptoUtil) 
+            :base(authService)
+		{
+			_loginService = loginService;
+	        _cryptoUtil = cryptoUtil;
+	        _personRepository = personRepository;
+		    _authenticationService = authenticationService;           
+		}
+
+		public ActionResult Index()
+		{
+		    var numberOfUsers = getNumberOfUsers();            
+		    ViewData["ShowFirstTimeRegisterLink"] = (numberOfUsers == 0);            
+
+			return RenderView("loginform");
+		}
+
+	    public ActionResult Process(string email, string password, string redirectUrl)
+		{
+			if (_loginService.VerifyAccount(email, password))
+			{
+			    var person = _personRepository.FindByEmail(email);
+				_authenticationService.SignIn(person);
+
+                if(redirectUrl != null)
+			        return new UrlRedirectResult(redirectUrl);
+
+			    return RedirectToAction("current", "conference");
+			}
+
+            //login failed
+	        TempData[TempDataKeys.Error] = "Invalid login";            
+	        return RedirectToAction("index");
+		}
+
+        [PostOnly]
+	    public ActionResult CreateAdminAccount(string firstName, string lastName, string email, string password, string passwordConfirm)
+        {
+            if (getNumberOfUsers() > 0)
+            {
+                throw new SecurityException(
+                    "This action is only valid when there are no registered users in the system.");
+            }
+
+            var task = new CreateAdminAccountTask(_personRepository, _cryptoUtil, firstName, 
+                lastName, email, password, passwordConfirm);
+
+            task.Execute();
+
+            if (!task.Success)
+            {
+                TempData[TempDataKeys.Error] = task.ErrorMessage;
+            }
+        	  
+            return RedirectToAction("index");
+        }
+
+	    private int getNumberOfUsers()
+	    {
+	        return _personRepository.GetNumberOfUsers();
+	    }
+	}
+}
