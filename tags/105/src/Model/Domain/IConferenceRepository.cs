ef65b3f48d20ea2a0fcf641bb80aba3398d18746
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-05-31 15:30:38 +0000

    CCNET build 105

---
+++ tags/105/src/Model/Domain/IConferenceRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using CodeCampServer.Model.Domain;
+
+namespace CodeCampServer.Model.Domain
+{
+	public interface IConferenceRepository
+	{
+		Conference[] GetAllConferences();
+		Conference GetConferenceByKey(string key);
+		Conference GetFirstConferenceAfterDate(DateTime date);
+	    Conference GetMostRecentConference(DateTime date);
+		Conference GetById(Guid id);
+		void Save(Conference conference);
+	    bool ConferenceExists(string name, string key);
+	    bool ConferenceKeyAvailable(string key);
+	}
+}
