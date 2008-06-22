d37d9e90d730743d6930879e116727749b008afc
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-06-22 22:29:46 +0000

    CCNET build 109

---
+++ tags/109/src/UnitTests/TestHelperExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+using Castle.Windsor;
+using CodeCampServer.Model;
+using CodeCampServer.Model.Domain;
+using CodeCampServer.Model.Impl;
+using CodeCampServer.Model.Security;
+using CodeCampServer.Website;
+using CodeCampServer.Website.Controllers;
+using CodeCampServer.Website.Helpers;
+using CodeCampServer.Website.Security;
+
+namespace CodeCampServer.UnitTests
+{
+	public static class TestHelperExtensions
+	{
+		/// <summary>
+		/// Returns true if the method is marked with the [AdminOnly] attribute
+		/// </summary>
+		/// <param name="method">The method to inspect</param>
+		/// <returns>true/false</returns>
+		public static bool HasAdminOnlyAttribute(this MethodInfo method)
+		{
+			//the [AdminOnly] attribute requires windsor to be setup
+			IWindsorContainer container = new WindsorContainer();
+
+			IoC.InitializeWith(container);
+			IoC.Register<IHttpContextProvider, HttpContextProvider>();
+			IoC.Register<IUserSession, FakeUserSession>();
+
+			//TODO:  is there a way to do this that _doesn't_ instantiate the attribute class?  if so we can
+			//delete the windsor setup crap above
+			object[] attributes = method.GetCustomAttributes(typeof (AdminOnlyAttribute), true);
+
+			IoC.Reset();
+
+			return attributes.Length > 0;
+		}
+
+		public class FakeUserSession : IUserSession
+		{
+			public Person GetLoggedInPerson()
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public bool IsAdministrator
+			{
+				get { throw new System.NotImplementedException(); }
+			}
+
+			public void PushUserMessage(FlashMessage message)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public FlashMessage PopUserMessage()
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+	}
+}
---
+++ tags/109/src/Website/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Castle.Core;
+using CodeCampServer.Model;
+using CodeCampServer.Website.Helpers;
+using MvcContrib.Castle;
+using MvcContrib.ExtendedComponentController;
+
+namespace CodeCampServer.Website
+{
+	public class Global : HttpApplication
+	{
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			Log.EnsureInitialized();
+			RegisterMvcTypes();
+			ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(IoC.GetContainer()));
+			ComponentControllerBuilder.Current.SetComponentControllerFactory(
+				new IoCComponentControllerFactory(new WindsorDependencyResolver(IoC.GetContainer())));
+
+			setupRoutes();
+		}
+
+		public static void RegisterMvcTypes()
+		{
+			IoC.Register<IRouteConfigurator, RouteConfigurator>("route-configurator");
+
+			// TODO: windsor mass component registration
+			// http://www.kenegozi.com/Blog/2008/03/01/windsor-mass-component-registration.aspx
+			// http://mikehadlow.blogspot.com/2008/04/problems-with-mvc-framework.html
+
+			//add all controllers
+			foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
+			{
+				if (typeof (IController).IsAssignableFrom(type))
+				{
+					IoC.Register(type.Name.ToLower(), type, LifestyleType.Transient);
+				}
+				if (typeof (ComponentController).IsAssignableFrom(type))
+				{
+					IoC.Register(type.Name.ToLower(), type, LifestyleType.Transient);
+				}
+			}
+		}
+
+		private static void setupRoutes()
+		{
+			var configurator = IoC.Resolve<IRouteConfigurator>();
+			configurator.RegisterRoutes();
+		}
+	}
+}
