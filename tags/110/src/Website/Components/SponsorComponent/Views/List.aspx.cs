16f05c66b8105c3116ef05d4082934f795986dbd
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-06-22 22:43:59 +0000

    CCNET build 110

---
+++ tags/110/src/Website/Components/SponsorComponent/Views/List.aspx.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+using CodeCampServer.Model.Domain;
+
+namespace CodeCampServer.Website.Components.SponsorComponent.Views
+{
+	public partial class List : ViewPage<Sponsor[]>
+	{
+	}
+}
