fa66f3282ca174a1a11efcc268f0800c5d1a5f58
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-06-23 02:21:07 +0000

    CCNET build 112

---
+++ tags/112/src/UnitTests/Website/Controllers/AdminControllerTester.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+using CodeCampServer.Model;
+using CodeCampServer.Model.Domain;
+using CodeCampServer.Model.Security;
+using CodeCampServer.Website.Controllers;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+using IUserSession=CodeCampServer.Model.IUserSession;
+
+namespace CodeCampServer.UnitTests.Website.Controllers
+{
+    [TestFixture]
+    public class AdminControllerTester
+    {
+        private MockRepository _mocks;
+        private IUserSession userSession;
+        private IConferenceRepository _conferenceRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mocks = new MockRepository();
+            userSession = _mocks.DynamicMock<IUserSession>();
+            _conferenceRepository = _mocks.CreateMock<IConferenceRepository>();
+        }
+
+        [Test]
+        public void IndexActionRendersDefaultView()
+        {
+            var controller = getController();
+            var result = controller.Index() as ViewResult;
+
+            if(result == null) 
+                Assert.Fail("expected renderview");
+
+            Assert.That(result.ViewName, Is.Null);
+        }
+
+        private AdminController getController()
+        {
+            var controller = new AdminController(userSession)
+                       {                           
+                           ViewEngine = new ViewEngineStub()
+                       };
+
+            controller.ControllerContext = new ControllerContextStub(controller);
+            return controller;
+        }
+
+    }
+}
---
+++ tags/112/src/Website/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using CodeCampServer.Model;
+
+namespace CodeCampServer.Website.Controllers
+{
+	[AdminOnly]
+	public class AdminController : Controller
+	{
+		public AdminController(IUserSession userSession)
+			: base(userSession)
+		{
+		}
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Schedule()
+		{
+			return View();
+		}
+	}
+}
