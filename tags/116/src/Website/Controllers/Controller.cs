ba05734fae1183706d25f50a0f1e3dcd9f1225d1
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-07-18 16:48:42 +0000

    CCNET build 116

---
+++ tags/116/src/Website/Controllers/Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using CodeCampServer.Model;
+using CodeCampServer.Model.Presentation;
+using CodeCampServer.Website.Views;
+
+namespace CodeCampServer.Website.Controllers
+{
+	public abstract class Controller : System.Web.Mvc.Controller
+	{
+		private readonly IUserSession _userSession;
+
+		protected Controller(IUserSession userSession)
+		{
+			_userSession = userSession;
+		}
+
+		protected override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			LogAction(filterContext);
+			
+			if (_userSession.IsAdministrator)
+			{
+				ViewData.Add("ShouldRenderAdminPanel", true);
+			}
+			PreparePageTitle();
+
+			base.OnActionExecuting(filterContext);
+		}
+
+		private void LogAction(ActionExecutingContext filterContext)
+		{
+			if (filterContext != null)
+			{
+				var controller = (System.Web.Mvc.Controller) filterContext.Controller;
+				RouteData routeData = controller.ControllerContext.RouteData;
+				object actionName = routeData.Values["action"];
+				object controllerName = routeData.Values["controller"];
+				Log.Debug(this, string.Format("Executing action:{0} on controller:{1}", actionName, controllerName));
+			}
+		}
+
+		private void PreparePageTitle()
+		{
+			if (ViewData.Contains<Schedule>())
+			{
+				string conferenceName = ViewData.Get<Schedule>().Name;
+				ViewData.Add("PageTitle", conferenceName);
+			}
+			else
+			{
+				ViewData.Add("PageTitle", "Code Camp Server");
+			}
+		}
+	}
+}
