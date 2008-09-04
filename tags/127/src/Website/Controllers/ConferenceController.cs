ebe533e26c756d7e2cbc3fe2bb47f8d777c79763
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-09-04 02:53:58 +0000

    CCNET build 127

---
+++ tags/127/src/Website/Controllers/ConferenceController.cs
@@ -0,0 +1,204 @@
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using CodeCampServer.Model;
+using CodeCampServer.Model.Domain;
+using CodeCampServer.Model.Presentation;
+using MvcContrib;
+using MvcContrib.Attributes;
+using MvcContrib.Filters;
+
+namespace CodeCampServer.Website.Controllers
+{
+	public class ConferenceController : BaseController
+	{
+		private readonly IClock _clock;
+		private readonly IConferenceService _conferenceService;
+		private readonly IUserSession _userSession;
+		private readonly IConferenceRepository _conferenceRepository;
+
+		public ConferenceController(IConferenceRepository conferenceRepository,
+		                            IConferenceService conferenceService,
+		                            IUserSession userSession,
+		                            IClock clock) : base(userSession)
+		{
+			_conferenceRepository = conferenceRepository;
+			_conferenceService = conferenceService;
+			_userSession = userSession;
+			_clock = clock;
+		}
+
+		[DefaultAction]
+		public ActionResult Index(string conferenceKey)
+		{
+			Schedule schedule = getScheduledConference(conferenceKey);
+
+			//if this conference is still hidden from the public, then only show it to administrators
+			if (!schedule.Conference.PubliclyVisible && !_userSession.IsAdministrator)
+				return RedirectToAction("current", "conference");
+
+			ViewData.Add(schedule);
+
+			return View();
+		}
+
+		public ActionResult KeyCheck(string conferenceKey)
+		{
+			bool result = _conferenceRepository.ConferenceKeyAvailable(conferenceKey);
+		    return Json(result);
+		}
+
+		public ActionResult Current()
+		{
+			Conference conference = _conferenceService.GetCurrentConference();
+
+			//if there are no conferences, then this is likely an admin setting the
+			//site up for the first time
+			if (conference == null)
+				return RedirectToAction("index", "admin");
+
+			return RedirectToAction("index", "conference", new {conferenceKey = conference.Key});
+		}
+
+		[Authorize(Roles="Administrator")]
+		public ActionResult List()
+		{
+			Conference[] conferences = _conferenceRepository.GetAllConferences();
+			ViewData.Add(conferences);
+			return View();
+		}
+
+		public ActionResult PleaseRegister(string conferenceKey)
+		{
+			Schedule conference = getScheduledConference(conferenceKey);
+			ViewData.Add(conference);
+			return View("registerform");
+		}
+
+		public ActionResult Register(string conferenceKey, string firstName, string lastName, string email,
+		                             string website,
+		                             string comment, string password)
+		{
+			try
+			{
+				//register the attendee
+				Conference conference = getConferenceByKey(conferenceKey);
+				Person person = _conferenceService.RegisterAttendee(firstName, lastName, email, website, comment,
+				                                                    conference, password);
+
+				//sign them in
+				ViewData.Add(person).Add(new Schedule(conference, _clock, null, null));
+				return View("registerconfirm");
+			}
+			catch (Exception exc)
+			{
+				TempData[TempDataKeys.Error] = "An error occurred while registering your account.";
+				Log.Error(this, "An error occcurred while registering a user", exc);
+				return View("pleaseregister");
+			}
+		}
+
+		public ActionResult ListAttendees(string conferenceKey)
+		{			
+			Conference conference = getConferenceByKey(conferenceKey);
+			Person[] attendees = conference.GetAttendees();	
+
+			AttendeeListing[] listings = getListingsFromAttendees(attendees);
+
+			ViewData
+				.Add(new Schedule(conference, _clock, null, null))
+				.Add(listings);
+
+			return View();
+		}
+
+        [Authorize(Roles = "Administrator")]
+		public ActionResult New()
+		{
+			ViewData.Add(new Conference());
+			return View("edit");
+		}
+
+        [Authorize(Roles = "Administrator")]
+		[PostOnly]
+		public ActionResult Save(Guid conf_id, string conf_name, string conf_key, DateTime conf_start, DateTime? conf_end, string conf_desc, bool conf_enabled)
+		{
+            Conference conference;
+            if(conf_id == Guid.Empty)
+            {
+                if (_conferenceRepository.ConferenceExists(conf_name, conf_key))
+                {
+                    TempData[TempDataKeys.Error] = "A conference has already been created with that name or key";
+                    return RedirectToAction("edit");
+                }
+
+                conference = new Conference();
+            }
+            else
+            {
+                conference = _conferenceRepository.GetById(conf_id);
+                if(conference == null)
+                {
+                    TempData[TempDataKeys.Error] = "Conference not found.";
+                    return RedirectToAction("list");
+                }
+            }
+
+
+            conference.StartDate = conf_start;
+            conference.EndDate = conf_end.HasValue ? conf_end : conf_start;           
+            conference.Description = conf_desc;
+            conference.PubliclyVisible = conf_enabled;            
+
+			try
+			{
+				_conferenceRepository.Save(conference);
+				TempData[TempDataKeys.Message] = "The conference was created successfully.";
+				return RedirectToAction("list");
+			}
+			catch (Exception exc)
+			{
+				Log.Error("Error saving conference.", exc);
+				TempData[TempDataKeys.Error] = "There was an error saving the conference.  The error was: " + exc;
+				ViewData.Add("conference", conference);
+				return View("edit");
+			}
+		}
+
+        [Authorize(Roles = "Administrator")]
+		public ActionResult Edit(string conferenceKey)
+		{
+			Conference conference = getConferenceByKey(conferenceKey);
+			if (conference == null)
+			{
+				TempData[TempDataKeys.Error] = "Conference not found.";
+				return RedirectToAction("current", "conference");
+			}
+
+			ViewData.Add(conference);
+			return View("edit");
+		}
+
+		private Conference getConferenceByKey(string conferenceKey)
+		{
+			return _conferenceRepository.GetConferenceByKey(conferenceKey);
+		}
+
+		private Schedule getScheduledConference(string conferenceKey)
+		{
+			Conference conference = getConferenceByKey(conferenceKey);
+			return new Schedule(conference, _clock, null, null);
+		}
+
+		private AttendeeListing[] getListingsFromAttendees(Person[] attendees)
+		{
+			var listings = new List<AttendeeListing>();
+			foreach (Person attendee in attendees)
+			{
+				listings.Add(new AttendeeListing(attendee));
+			}
+
+			return listings.ToArray();
+		}
+	}
+}
---
+++ tags/127/src/Website/Views/Conference/Edit.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Layouts/Admin.Master" AutoEventWireup="true" Inherits="System.Web.Mvc.ViewPage" Title="Code Camp Server" %>
+<%@ Import Namespace="System.Collections.Generic"%>
+<%@ Import Namespace="CodeCampServer.Website.Controllers"%>
+<%@ Import namespace="MvcContrib"%>
+<%@ Import namespace="CodeCampServer.Model.Domain"%>
+<%@ Import Namespace="System.Web.Mvc" %>
+<%@ Import Namespace="Microsoft.Web.Mvc" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server"> 
+  <script type="text/javascript">
+    $(document).ready( function() {
+      $('#conf_start').datepicker();
+      $('#conf_end').datepicker();
+      
+      $('#conf_save').click( function() {
+        var start = $('#conf_start').value;
+      });
+      
+      jQuery.validator.addMethod("uniqueKey", function(value, element) {             
+            return !checkKey(value);
+        }, "This URL Key has already been used");      
+        
+      $("form#new_conference").validate({
+        errorContainer : '#errors',
+        errorClass: "formError",
+        messages: {
+          conf_name: "Required",
+          conf_key: "Required/Must be unique",
+          conf_start: "Required",
+          conf_end: "Required",
+          conf_descr: "Required"
+        }        
+      });           
+      
+    });
+    
+    function checkKey(key) {        
+        $.ajax({
+            async: false,
+            method: 'GET',
+            url: '<%= Url.Action("keycheck") %>',
+            data: { key : key },
+            dataType:"json",
+            beforeSend: function() { $("#indicator").show(); },
+            complete: function() {  $("#indicator").hide(); },
+            success: function(result) {                
+                return result;              
+            }
+        });
+    }
+    
+  </script>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContentPlaceHolder" runat="server">    
+            
+      <p><%= Html.ActionLink<ConferenceController>(c => c.List(), "« Cancel") %></p>
+      
+      <div id="errors"></div>
+      
+      <% var  conference = ViewData.Get<Conference>();%>
+            
+      <form method="post" id="new_conference" action='<%= Url.Action("save") %>'>
+        <fieldset>
+            <legend>New Conference</legend>
+            
+            <%= Html.Hidden("conf_id", conference.Id.ToString()) %>
+            
+            <label for="conf_name">Name</label>
+            
+            <%= Html.TextBox("conf_name", conference.Name ?? "", new { @class="required", size="40", maxLength="100"}) %>
+            <span class="info">The name of the conference.</span>
+            
+            <label for="conf_key">Unique Key</label>
+            <%= Html.TextBox("conf_key", conference.Key ?? "", new { @class = "uniqueKey required", size = "25", maxLength = "25" })%>
+            <span class="info">A unique name to identify the conference.  Will be used in a url, so it must not contain illegal characters such as spaces or symbols.</span>
+            
+            <label for="conf_start">Starts</label>        
+            <%= Html.TextBox("conf_start", conference.StartDate.HasValue ? 
+                          conference.StartDate.Value.ToShortDateString() : "",
+                                          new { @class = "required", size = "25", maxLength = "10" })%>
+            
+            <label for="conf_end">Ends</label>        
+            <%= Html.TextBox("conf_end", conference.EndDate.HasValue ? 
+              conference.EndDate.Value.ToShortDateString() : "",
+                                          new { @class = "required", size = "25", maxLength = "10" })%>
+            
+            <label for="conf_descr">Description</label>
+            <%= Html.TextArea("conf_descr", conference.Description ?? "", 7, 60, new { @class = "required" })%>
+            <span class="info">Max 1000 characters.  No formatting.</span>
+            
+            <label for="conf_enabled">Visible to the public</label>
+            <%= Html.CheckBox("conf_enabled", conference.PubliclyVisible) %>
+            
+            <div class="button-row">
+                <input type="submit" id="conf_save" value="Save" class="submit" />
+            </span>
+        </fieldset>    
+    </form>
+
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="SidebarContentPlaceHolder" runat="server">
+</asp:Content>
