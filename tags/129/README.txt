54dbcbe5b87dc2ea02368ac1a7dd4fb9aa51dd13
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-09-16 12:58:54 +0000

    CCNET build 129

---
+++ tags/129/README.txt
@@ -0,0 +1,13 @@
+http://codecampserver.org
+Apache License 2.0
+
+To build and run this project, execute "clicktobuild.bat".  You must have the .Net Framework 3.5, .Net 2.0 SDK, and SQL Express (TcpIp,MixedMode enabled) installed.
+*NOTE: You can set machine specific settings by copying /config/local-properties.xml.template to /config/local-properties.xml, editing the .xml file, and then running "firsttimeconfig.bat".
+The solution works in Visual Studio 2008.
+
+Join the discussion list:  http://groups.google.com/group/codecampserver-discuss
+
+Thank you!  
+Jeffrey Palermo
+project founder
+Email:  jeffrey at palermo d0t cc
---
+++ tags/129/src/Model/Domain/TimeSlot.cs
@@ -0,0 +1,68 @@
+using System;
+using Iesi.Collections.Generic;
+using System.Collections.Generic;
+
+namespace CodeCampServer.Model.Domain
+{
+    public class TimeSlot : EntityBase
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private string _purpose;
+        private Conference _conference;
+        private ISet<Session> _sessions = new HashedSet<Session>();
+
+        public TimeSlot()
+        {
+        }
+
+        public TimeSlot(DateTime startTime, DateTime endTime, string purpose)
+        {
+            _startTime = startTime;
+            _endTime = endTime;
+            _purpose = purpose;
+        }
+
+        public TimeSlot(Conference conference, DateTime startTime, DateTime endTime, string purpose)
+        {
+            _conference = conference;
+            _startTime = startTime;
+            _endTime = endTime;
+            _purpose = purpose;
+        }
+
+        public virtual DateTime StartTime
+        {
+            get { return _startTime; }
+            set { _startTime = value; }
+        }
+
+        public virtual DateTime EndTime
+        {
+            get { return _endTime; }
+            set { _endTime = value; }
+        }
+
+        public virtual string Purpose
+        {
+            get { return _purpose; }
+            set { _purpose = value; }
+        }
+
+        public virtual Conference Conference
+        {
+            get { return _conference; }
+            set { _conference = value; }
+        }
+
+        public virtual void AddSession(Session session)
+        {
+            _sessions.Add(session);
+        }
+
+    	public virtual Session[] GetSessions()
+    	{
+    		return new List<Session>(_sessions).ToArray();
+    	}
+    }
+}
