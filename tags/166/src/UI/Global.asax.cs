c82c722ff1106ae18c52267ead5667537c8059a8
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2008-12-16 21:48:46 +0000

    CCNET build 166

---
+++ tags/166/src/UI/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CodeCampServer.UI.Models;
+
+namespace CodeCampServer.UI
+{
+	public class GlobalApplication : HttpApplication
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.gif/{*pathInfo}");
+
+			routes.MapRoute("Home", "home", new {controller = "home", action = "index"});
+            
+			routes.MapRoute(
+				"Default", // Route name
+				"{controller}/{action}", // URL with parameters
+				new {controller = "home", action = "index"} // Parameter defaults
+				);
+		}
+
+		protected void Application_Start()
+		{
+			RegisterRoutes(RouteTable.Routes);
+            AutoMapperConfiguration.Configure();
+		}
+	}
+}
