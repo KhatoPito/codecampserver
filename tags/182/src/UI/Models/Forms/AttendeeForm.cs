6cd37402943bf606d49eb13a1d14bbfdf57798fc
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-01-08 21:40:08 +0000

    CCNET build 182

---
+++ tags/182/src/UI/Models/Forms/AttendeeForm.cs
@@ -0,0 +1,17 @@
+using System;
+using CodeCampServer.Core.Domain.Model;
+using CodeCampServer.Core.Messages;
+
+namespace CodeCampServer.UI.Models.Forms
+{
+    public class AttendeeForm : IAttendeeMessage
+    {
+        public Guid ConferenceID { get; set; }
+        public Guid? AttendeeID{ get; set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public AttendanceStatus Status { get; set; }
+        public string Webpage { get; set; }
+    }
+}
