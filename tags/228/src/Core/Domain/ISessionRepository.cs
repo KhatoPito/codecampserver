1bbe10d902dbd7fcaee66c172186e6efb65af147
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-01-15 15:47:15 +0000

    CCNET build 228

---
+++ tags/228/src/Core/Domain/ISessionRepository.cs
@@ -0,0 +1,11 @@
+using CodeCampServer.Core.Domain.Model;
+
+namespace CodeCampServer.Core.Domain
+{
+	public interface ISessionRepository : IKeyedRepository<Session>
+	{
+		Session[] GetAllForConference(Conference conference);
+		Session[] GetAllForTimeSlot(TimeSlot timeSlot);
+		Session[] GetAllForTrack(Track track);
+	}
+}
