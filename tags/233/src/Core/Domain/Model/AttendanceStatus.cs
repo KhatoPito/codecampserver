65b770fa5980cafaca658540e4d9a87ee45afc63
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-01-21 17:48:01 +0000

    CCNET build 233

---
+++ tags/233/src/Core/Domain/Model/AttendanceStatus.cs
@@ -0,0 +1,19 @@
+using Tarantino.Core.Commons.Model.Enumerations;
+
+namespace CodeCampServer.Core.Domain.Model
+{
+	public class AttendanceStatus : Enumeration
+	{
+		public AttendanceStatus()
+		{
+		}
+
+		public AttendanceStatus(string displayName, int value) : base(value, displayName)
+		{
+		}
+
+		public static AttendanceStatus Interested = new AttendanceStatus("Not Attending", 1);
+		public static AttendanceStatus Attending = new AttendanceStatus("Attending", 2);
+		public static AttendanceStatus Confirmed = new AttendanceStatus("Confirmed", 3);
+	}
+}
---
+++ tags/233/src/UI/Controllers/SessionController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using CodeCampServer.Core.Domain;
+using CodeCampServer.Core.Domain.Model;
+using CodeCampServer.UI.Helpers.Filters;
+using CodeCampServer.UI.Helpers.Mappers;
+using CodeCampServer.UI.Models.Forms;
+
+namespace CodeCampServer.UI.Controllers
+{
+	[RequiresConferenceFilter]
+	public class SessionController : SaveController<Session, SessionForm>
+	{
+		private readonly ISessionMapper _mapper;
+		private readonly ISessionRepository _repository;
+
+		public SessionController(ISessionRepository repository, ISessionMapper mapper) : base(repository, mapper)
+		{
+			_repository = repository;
+			_mapper = mapper;
+		}
+
+		public ViewResult New()
+		{
+			return View("Edit", new SessionForm());
+		}
+
+		public ViewResult Index(Session session)
+		{
+			return View(_mapper.Map(session));
+		}
+
+		public ViewResult List(Conference conference)
+		{
+			Session[] sessions = _repository.GetAllForConference(conference);
+			return View(_mapper.Map(sessions));
+		}
+
+		public ViewResult Edit(Session session)
+		{
+			return View(_mapper.Map(session));
+		}
+
+		[ValidateModel(typeof (SessionForm))]
+		public ActionResult Save([Bind(Prefix = "")] SessionForm form, string urlreferrer)
+		{
+			Func<ActionResult> successRedirect = GetSuccessRedirect(form, urlreferrer);
+			return ProcessSave(form, successRedirect);
+		}
+
+		private Func<ActionResult> GetSuccessRedirect(SessionForm form, string urlreferrer)
+		{
+			Func<ActionResult> successRedirect = () => RedirectToIndex(form.Conference);
+			if (!String.IsNullOrEmpty(urlreferrer))
+			{
+				successRedirect = () => Redirect(urlreferrer);
+			}
+			return successRedirect;
+		}
+
+		protected override IDictionary<string, string[]> GetFormValidationErrors(SessionForm form)
+		{
+			var result = new ValidationResult();
+			if (KeyAlreadyExists(form))
+			{
+				result.AddError<SessionForm>(x => x.Key, "This session key already exists");
+			}
+			return result.GetAllErrors();
+		}
+
+		private bool KeyAlreadyExists(SessionForm message)
+		{
+			Session session = _repository.GetByKey(message.Key);
+			return session != null && session.Id != message.Id;
+		}
+
+		public RedirectToRouteResult Delete(Session session)
+		{
+			_repository.Delete(session);
+			return RedirectToIndex(session.Conference);
+		}
+
+		private RedirectToRouteResult RedirectToIndex(Conference conference)
+		{
+			return RedirectToAction<SessionController>(x => x.Index(null), new {conference = conference.Key});
+		}
+	}
+}
