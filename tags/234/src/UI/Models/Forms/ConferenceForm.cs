59f78ea7c6f303c093c84d89153ad0cb7a257aec
codecampserver <codecampserver@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-01-22 15:07:04 +0000

    CCNET build 234

---
+++ tags/234/src/UI/Models/Forms/ConferenceForm.cs
@@ -0,0 +1,45 @@
+using System;
+using Castle.Components.Validator;
+using CodeCampServer.UI.Helpers.Validation.Attributes;
+using CodeCampServer.UI.Models.Forms.Attributes;
+using CodeCampServer.UI.Models.Validation.Attributes;
+
+namespace CodeCampServer.UI.Models.Forms
+{
+	public class ConferenceForm
+	{
+		[BetterValidateNonEmpty("Conference Key")]
+		[ValidateRegExp(@"^[A-Za-z0-9\-]+$", "Key should only contain letters, numbers, and hypens.")]
+		public virtual string Key { get; set; }
+
+		public virtual Guid Id { get; set; }
+
+		[BetterValidateNonEmpty("Name")]
+		public virtual string Name { get; set; }
+
+		public virtual string Description { get; set; }
+
+		[BetterValidateDate("Start Date")]
+		public virtual string StartDate { get; set; }
+
+		[BetterValidateDate("End Date")]
+		public virtual string EndDate { get; set; }
+
+		[BetterValidateNonEmpty("Location")]
+		public virtual string LocationName { get; set; }
+
+		public virtual string Address { get; set; }
+
+		public virtual string City { get; set; }
+
+		[Label("State")]
+		public virtual string Region { get; set; }
+
+		[Label("Zip Code")]
+		public virtual string PostalCode { get; set; }
+
+		[Label("Phone Number")]
+		public virtual string PhoneNumber { get; set; }
+
+	}
+}
---
+++ tags/234/src/UI/Views/Schedule/ScheduleSlot.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"
+	Inherits="CodeCampServer.UI.Helpers.ViewPage.ScheduleSlotView" %>
+<%@ Import Namespace="CodeCampServer.Core.Common"%>
+<%@ Import Namespace="CodeCampServer.UI.Controllers"%>
+<%@ Import Namespace="CodeCampServer.UI.Models.Forms"%>
+
+	<% foreach (SessionForm session in ViewData.Model) { %>
+	<div class="session">
+		<%Html.RenderPartial("EditSessionLink",session,ViewData); %>
+		<span><a href="<%=Url.Action<SessionController>(c=>c.Index(null),new{session=session.Id})%>"><%=session.Title %></a></span><span>(Room: <%=session.RoomNumber %>)</span>
+	</div>			
+	<% } %>
