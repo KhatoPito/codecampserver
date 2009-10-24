ad65e267be1a5809051f8ae257dd0ce57fcb251c
mhinze <mhinze@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2009-10-24 05:46:22 +0000

    fluent nhibernate

---
+++ branches/fnh/src/Infrastructure/DataAccess/Mappings/EntityClassMap.cs
@@ -0,0 +1,17 @@
+﻿using CodeCampServer.Core.Domain.Model;
+using FluentNHibernate.Mapping;
+
+namespace CodeCampServer.Infrastructure.DataAccess.Mappings
+{
+	public class EntityClassMap<TEntity> : ClassMap<TEntity>
+		where TEntity : PersistentObject
+	{
+		public EntityClassMap()
+		{
+			Cache.ReadWrite();
+			DynamicUpdate();
+			Id(x => x.Id)
+				.GeneratedBy.GuidComb();
+		}
+	}
+}
