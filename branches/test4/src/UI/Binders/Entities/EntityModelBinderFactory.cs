99f36164cda16cc4db112e76a6cc9eb32c846e3e
erichexter <erichexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2010-01-09 16:26:16 +0000

    Creating Branch test4

---
+++ branches/test4/src/UI/Binders/Entities/EntityModelBinderFactory.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Web.Mvc;
+using CodeCampServer.Core;
+using CodeCampServer.Core.Domain;
+using CodeCampServer.Core.Domain.Bases;
+
+namespace CodeCampServer.UI.Binders.Entities
+{
+	public interface IEntityModelBinderFactory
+	{
+		IEntityModelBinder GetEntityModelBinder(Type entityType);
+	}
+
+	public class EntityModelBinderFactory : AbstractFactoryBase<IEntityModelBinder>, IEntityModelBinderFactory
+	{
+		public static Func<Type, IEntityModelBinder> GetDefault = modelBinderType => DefaultUnconfiguredState();
+
+		public IEntityModelBinder GetEntityModelBinder(Type entityType)
+		{
+			Type repositoryType = typeof (IRepository<>).MakeGenericType(entityType);
+			Type modelBinderType = typeof (ModelBinder<,>).MakeGenericType(entityType, repositoryType);
+
+			return GetDefault(modelBinderType);
+		}
+	}
+}
