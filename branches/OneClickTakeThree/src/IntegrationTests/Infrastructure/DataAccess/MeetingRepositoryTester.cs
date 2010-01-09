89749d2037053a658942b2e9a76d06d5747f8cf6
erichexter <erichexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2010-01-09 20:40:14 +0000

    Creating Branch OneClickTakeThree

---
+++ branches/OneClickTakeThree/src/IntegrationTests/Infrastructure/DataAccess/MeetingRepositoryTester.cs
@@ -0,0 +1,15 @@
+using CodeCampServer.Core.Domain.Model;
+using CodeCampServer.Infrastructure.NHibernate.DataAccess.Impl;
+using NUnit.Framework;
+
+namespace CodeCampServer.IntegrationTests.Infrastructure.DataAccess
+{
+	[TestFixture]
+	public class MeetingRepositoryTester : RepositoryTester<Meeting, MeetingRepository>
+	{
+		protected override MeetingRepository CreateRepository()
+		{
+			return GetInstance<MeetingRepository>();
+		}
+	}
+}
