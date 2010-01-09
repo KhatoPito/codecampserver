dae1a17914ac37960797cc4ad5cb4e48353fb605
erichexter <erichexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2010-01-09 17:40:20 +0000

    Creating Branch autobranchtest

---
+++ branches/autobranchtest/src/UI/Models/Display/MeetingAnnouncementDisplay.cs
@@ -0,0 +1,26 @@
+using System;
+using CodeCampServer.Core.Domain.Model;
+using CodeCampServer.UI.Helpers.Attributes;
+using MvcContrib.UI.InputBuilder.Attributes;
+
+namespace CodeCampServer.UI.Models.Input
+{
+	public class MeetingAnnouncementDisplay : IKeyable, IGloballyUnique
+	{
+		public Guid Id { get; set; }
+		public string Key { get; set; }
+		public string Heading { get; set; }
+		public string Topic { get; set; }
+		public string Summary { get; set; }
+		public DateTimeSpan When { get; set; }
+		public string LocationName { get; set; }
+		public string LocationUrl { get; set; }
+
+		[Label("Speaker")]
+		public string SpeakerName { get; set; }
+
+		public string SpeakerUrl { get; set; }
+		public string SpeakerBio { get; set; }
+		public string MeetingInfo { get; set; }
+	}
+}
