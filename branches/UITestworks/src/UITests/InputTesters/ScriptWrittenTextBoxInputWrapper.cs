2cbd1e2effe0f87d73c955af1c47724c8a5ae294
eric.hexter <eric.hexter@25cf9c4d-fa41-0410-a78d-8fe958b11fb0>
2010-03-03 20:06:36 +0000

    updated the ui tests

--- branches/UITestworks/src/UITests/InputTesters/ScriptWrittenTextBoxInputWrapper.cs
+++ branches/UITestworks/src/UITests/InputTesters/ScriptWrittenTextBoxInputWrapper.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Linq.Expressions;
+using MvcContrib.TestHelper.Ui;
 using MvcContrib.UI.InputBuilder.Helpers;
 
 namespace UITestHelper
 {
-    public class ScriptWrittenTextBoxInputWrapper : BaseInputWrapper<string>
+    public class ScriptWrittenTextBoxInputWrapper : InputTesterBase<string>
     {
         public ScriptWrittenTextBoxInputWrapper(string name, string value) : base(value, name) {}
 
